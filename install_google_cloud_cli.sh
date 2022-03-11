af31b3d6ce11fcfca733142b6b3cf56d0ea87836
Vital Lobachevskij <vlobachevsky@gmail.com>
2022-03-11 12:48:07 +0000

    Add scripts

---
+++ install_google_cloud_cli.sh
@@ -0,0 +1,12 @@
+#!/bin/bach
+
+sudo apt-get install apt-transport-https ca-certificates gnupg -y
+
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+sudo apt-get update && sudo apt-get install google-cloud-cli -y
+
+
--- prepare_for_terraform.sh
+++ prepare_for_terraform.sh
@@ -2,3 +2,7 @@
 
 gcloud config set project $PROJECT_ID
 gcloud iam service-accounts create terraform --display-name="Terraform service account"
+gcloud iam service-accounts keys create ~/terraform-key.json --iam-account=terraform@$PROJECT_ID.iam.gserviceaccount.com
+
+# Create bucket to keep Terraform state
+gsutil mb gs://$PROJECT_ID-tfstate
