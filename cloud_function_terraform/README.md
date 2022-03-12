58ce112685a41ca8f49a4f2d336f45cd39d6157d
Vital Lobachevskij <vlobachevsky@gmail.com>
2022-03-12 15:18:31 +0000

    - Fix prepare_for_terraform script - Add new project folder

---
+++ cloud_function_terraform/README.md
@@ -0,0 +1,1 @@
+# lab-cloud-function
--- prepare_for_terraform.sh
+++ prepare_for_terraform.sh
@@ -9,7 +9,7 @@ gsutil rb -f gs://$PROJECT_ID-tfstate
 
 gcloud iam service-accounts create terraform --display-name="Terraform service account"
 gcloud iam service-accounts keys create ~/terraform-key.json --iam-account=terraform@$PROJECT_ID.iam.gserviceaccount.com
-gsutil iam ch serviceAccount:terraform@$PROJECT_ID.iam.gserviceaccount.com:roles/storage.admin gs://$PROJECT_ID-tfstate
 
 # Create bucket to keep Terraform state
 gsutil mb gs://$PROJECT_ID-tfstate
+gsutil iam ch serviceAccount:terraform@$PROJECT_ID.iam.gserviceaccount.com:roles/storage.admin gs://$PROJECT_ID-tfstate
