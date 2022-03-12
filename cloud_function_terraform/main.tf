ec4edc4cf9a57203f14363a520f19d1ea120d1ad
Vital Lobachevskij <vlobachevsky@gmail.com>
2022-03-12 16:03:48 +0000

    - Use required_providers

--- cloud_function_terraform/main.tf
+++ cloud_function_terraform/main.tf
@@ -1,7 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
 provider "google" {
-  version     = "3.5.0"
   credentials = file(var.credentials_file)
-  project     = var.project
-  region      = var.region
-  zone        = var.zone
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
