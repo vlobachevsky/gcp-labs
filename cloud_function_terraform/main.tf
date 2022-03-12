11ab526869f64a31b79d45c7455035438568c468
Vital Lobachevskij <vlobachevsky@gmail.com>
2022-03-12 15:54:16 +0000

    - Use variables for some hardcoded parts

--- cloud_function_terraform/main.tf
+++ cloud_function_terraform/main.tf
@@ -1,18 +1,7 @@
-terraform {
-  backend "gcs" {
-    bucket      = "playground-s-11-3c127cb4-tfstate"
-    credentials = "/home/cloud_user/terraform-key.json"
-  }
-}
-
-locals {
-  project = "playground-s-11-3c127cb4"
-}
-
 provider "google" {
   version     = "3.5.0"
-  credentials = file("/home/cloud_user/terraform-key.json")
-  project     = locals.project
-  region      = "us-central1"
-  zone        = "us-central1-c"
+  credentials = file(var.credentials_file)
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
 }
---
+++ cloud_function_terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+project          = "playground-s-11-3c127cb4"
+credentials_file = "/home/cloud_user/terraform-key.json"
