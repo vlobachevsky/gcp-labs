bb6300ea3c0bddbe276bef2adfefe16026f5c3ed
Vital Lobachevskij <vlobachevsky@gmail.com>
2022-03-14 08:02:54 +0000

    - Disable creating bucket for remote state

--- prepare_for_terraform.sh
+++ prepare_for_terraform.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+gcloud auth login
 gcloud config set project $PROJECT_ID
 
 # Clean up
 gcloud iam service-accounts delete terraform@$PROJECT_ID.iam.gserviceaccount.com
-gsutil rb -f gs://$PROJECT_ID-tfstate
+# gsutil rb -f gs://$PROJECT_ID-tfstate
 
 
 gcloud iam service-accounts create terraform --display-name="Terraform service account"
 gcloud iam service-accounts keys create ~/terraform-key.json --iam-account=terraform@$PROJECT_ID.iam.gserviceaccount.com
 
 # Create bucket to keep Terraform state
-gsutil mb gs://$PROJECT_ID-tfstate
-gsutil iam ch serviceAccount:terraform@$PROJECT_ID.iam.gserviceaccount.com:roles/storage.admin gs://$PROJECT_ID-tfstate
+# gsutil mb gs://$PROJECT_ID-tfstate
+# gsutil iam ch serviceAccount:terraform@$PROJECT_ID.iam.gserviceaccount.com:roles/storage.admin gs://$PROJECT_ID-tfstate
